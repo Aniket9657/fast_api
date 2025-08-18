c9f6d9aaa91c23f3b062e47fd86997f82c7c798a
Aniket9657 <124755066+Aniket9657@users.noreply.github.com>
2025-08-18 23:11:40 +0530

    Practicing Fun in py

--- pro4.py
+++ pro4.py
@@ -1,9 +1,21 @@
-def even(n):
-    if n%2==0:
-        return True
-    else:
-        return False 
+# def even(n):
+#     if n%2==0:
+#         return True
+#     else:
+#         return False 
     
 
 
-print(even(100))    
+# print(even(100))    
+
+
+def prime (n):
+    if n<2:
+        return False
+    for i in range(2,n):
+        if n%i==0:
+            return False
+        return True 
+
+
+print(prime(7))
