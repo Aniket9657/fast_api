c53f37e9ad58f781dfa37f7678f57e03ee9b1176
Aniket9657 <124755066+Aniket9657@users.noreply.github.com>
2025-08-18 13:09:53 +0530

    Practice

--- pro3.py
+++ pro3.py
@@ -23,3 +23,37 @@
 # words = ['Bill','Fun','Run']
 # for i in words:
 #     print(i,len(i))
+
+
+# # code that modifies a collection while iterating over that same collection 
+# #Sample collection 
+# users ={'Aniket': 'active', 'Rakesh':'inactive', 'suresh':'inactive','Kamlesh':'active'}
+
+# #Iterate over a copy 
+# for user,status in users.copy().items():
+#     if status == 'inactive':
+#         del users[user]
+
+
+# #create a new collection 
+# active_user ={}
+# for user,status in users.items():
+#     if status == 'active':
+#         active_user[user] = status         
+
+# print("Active Users:",active_user)
+# for i in range(10):
+#      print(i/2)
+
+# def add(x,y):
+#     return x+y
+
+# print(add(2,4))
+
+
+# a=[1,2,3,4,5,2]
+# a.append(2)
+# print(a)
+
+# print(a.count(2))
+
